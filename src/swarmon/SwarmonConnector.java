07782fee22eda7310ffaf0ea6306fa65a08ec613
Kamil Mrowiec <kaamilm@gmail.com>
2013-09-04 07:33:05 +0100

    Swarmon API - work in progress

--- src/swarmon/SwarmonConnector.java
+++ src/swarmon/SwarmonConnector.java
@@ -1,9 +1,13 @@
 package swarmon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -21,7 +25,7 @@ public class SwarmonConnector {
 	
 	public SwarmonConnector(String username, String key){
 		client = new DefaultHttpClient();
-		API_URL_BASE = username + ":" + key + API_URL_BASE;
+		API_URL_BASE = "http://" + username + ":" + key + API_URL_BASE;
 	}
 	
 	public void createRobot(String teamName, String robotName) throws UnsupportedEncodingException{
@@ -49,6 +53,23 @@ public class SwarmonConnector {
 		params.add(new BasicNameValuePair("name", teamName));
 		params.add(new BasicNameValuePair("website", website));
 		post.setEntity(new UrlEncodedFormEntity(params));
+		
+	}
+	
+	public void getTeams(){
+		HttpPost post = new HttpPost(API_URL_BASE + "/teams");
+		try{
+			HttpResponse resp = client.execute(post);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+			  String line = "";
+			  while ((line = rd.readLine()) != null) {
+			   System.out.println(line);
+			  }
+
+		}catch(IOException ex){
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
 		
 	}
 }
--- src/swarmon/SwarmonTest.java
+++ src/swarmon/SwarmonTest.java
@@ -2,8 +2,9 @@ package swarmon;
 
 public class SwarmonTest {
 
-	public static void main(){
+	public static void main(String[] args){
 		SwarmonConnector swarmon = new SwarmonConnector("AberST", "api26test1");
+		swarmon.getTeams();
 		
 	}
 	
